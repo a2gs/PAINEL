a21829497a1242600484beccc407649ae44ded89
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-02-09 20:24:01 -0200

    Update killAll.sh

--- scripts/killAll.sh
+++ scripts/killAll.sh
@@ -17,5 +17,10 @@
 
 #kill `cat servlock`
 kill `ps -o pid -C servList | grep -v PID`
+ALERT_ERROR 'Kill servList'
+
 kill `ps aux | grep select| grep -v vim | grep -v grep | awk '{print $2}'`
+ALERT_ERROR 'Kill select'
+
 kill `ps -o pid -C serv | grep -v PID`
+ALERT_ERROR 'Kill serv'
