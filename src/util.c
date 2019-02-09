986929f532a5c355de6fbdac05c4594baf7a694c
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-02-09 20:14:19 -0200

    Add files via upload

---
+++ src/util.c
@@ -0,0 +1,276 @@
+/* Andre Augusto Giannotti Scota (a2gs)
+ * andre.scota@gmail.com
+ *
+ * PAINEL
+ *
+ * Public Domain
+ *
+ */
+
+
+/* sc.c
+ * Project's auxiliary functions (client/server).
+ *
+ *  Who     | When       | What
+ *  --------+------------+----------------------------
+ *   a2gs   | 13/08/2018 | Creation
+ *          |            |
+ */
+
+
+/* *** INCLUDES ************************************************************************ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "util.h"
+
+
+/* *** DEFINES AND LOCAL DATA TYPE DEFINATION ****************************************** */
+#define LOGPATH_SZ		(350)
+
+
+/* *** LOCAL PROTOTYPES (if applicable) ************************************************ */
+
+
+/* *** EXTERNS / LOCAL / GLOBALS VARIEBLES ********************************************* */
+static char logPath[LOGPATH_SZ + 1] = {0};
+
+
+/* *** FUNCTIONS *********************************************************************** */
+char * getPAINELEnvHomeVar(void)
+{
+	return(getenv(PAINEL_HOME_ENV));
+}
+
+char * time_DDMMYYhhmmss(void)
+{
+	static char segmil[DATA_LEN + 1] = {'\0'};
+	time_t rawtime;
+	struct tm *tmTime = NULL;
+
+	rawtime = time(0);
+	tmTime = localtime(&rawtime);
+	strftime((char *)segmil, DATA_LEN, "%d/%m/%y %H:%M:%S", tmTime);
+
+	segmil[DATA_LEN - 1]='\0';
+
+	return(segmil);
+}
+
+/* ---[DUAL HTML]------------------------------------------------------------------------------------------ */
+int html_fopen(htmlFiles_t *htmls, char *htmlStatic, char *htmlRefresh)
+{
+	/* Opening static */
+	htmls->htmlStatic = fopen(htmlStatic, "w");
+	if(htmls->htmlStatic == NULL){
+		fprintf(stderr, "Erro criando HTML STATIC [%s]. Motivo [%s]\n", htmlStatic, strerror(errno));
+		return(NOK);
+	}
+
+	htmls->fdStatic = fileno(htmls->htmlStatic);
+	if(htmls->fdStatic == -1){
+		fprintf(stderr, "Erro em pegar descritor de HTML STATIC [%s]. Motivo: [%s]\n", htmlStatic, strerror(errno));
+		return(NOK);
+	}
+
+	/* Opening refresh */
+	htmls->htmlRefresh = fopen(htmlRefresh, "w");
+	if(htmls->htmlRefresh == NULL){
+		fprintf(stderr, "Erro criando HTML REFRESH [%s]. Motivo: [%s]\n", htmlRefresh, strerror(errno));
+		return(NOK);
+	}
+
+	htmls->fdRefresh = fileno(htmls->htmlRefresh);
+	if(htmls->fdRefresh == -1){
+		fprintf(stderr, "Erro em pegar descritor de HTML REFRESH [%s]. Motivo: [%s]\n", htmlRefresh, strerror(errno));
+		return(NOK);
+	}
+
+	/* Locking */
+	if(lockf(htmls->fdStatic, F_LOCK, 0) < 0){
+		fprintf(stderr, "Nao foi possivel fazer lock no arquivo HTML STATIC [%s]. Motivo: [%s]\n", htmlStatic, strerror(errno));
+		return(NOK);
+	}
+
+	if(lockf(htmls->fdRefresh, F_LOCK, 0) < 0){
+		fprintf(stderr, "Nao foi possivel fazer lock no arquivo HTML REFRESH [%s]. Motivo: [%s]\n", htmlRefresh, strerror(errno));
+		return(NOK);
+	}
+
+	return(OK);
+}
+
+int html_writeDual(htmlFiles_t *htmls, int files, char *msg)
+{
+	if(files == 0){
+		fprintf(htmls->htmlStatic, "%s", msg);
+		fprintf(htmls->htmlRefresh, "%s", msg);
+	}
+	else if(files == 1) fprintf(htmls->htmlStatic,  "%s", msg);
+	else if(files == 2) fprintf(htmls->htmlRefresh, "%s", msg);
+
+	return(OK);
+}
+
+int html_fflush(htmlFiles_t *htmls)
+{
+	fflush(htmls->htmlStatic );
+	fflush(htmls->htmlRefresh);
+	return(OK);
+}
+
+int html_fclose(htmlFiles_t *htmls)
+{
+	/* Unlocking */
+	if(lockf(htmls->fdStatic, F_ULOCK, 0) < 0){
+		fprintf(stderr, "Nao foi possivel fazer o unlock do arquivo HTML STATIC: [%s]\n", strerror(errno));
+		return(NOK);
+	}
+
+	if(lockf(htmls->fdRefresh, F_ULOCK, 0) < 0){
+		fprintf(stderr, "Nao foi possivel fazer o unlock do arquivo HTML STATIC: [%s]\n", strerror(errno));
+		return(NOK);
+	}
+
+	/* Closing */
+	fclose(htmls->htmlStatic);
+	fclose(htmls->htmlRefresh);
+
+	memset(htmls, 0, sizeof(htmlFiles_t));
+
+	return(OK);
+}
+
+int html_testHtmlLock(FILE *file)
+{
+	int fd = 0;
+
+	fd = fileno(file);
+	if(fd == -1){
+		fprintf(stderr, "Erro fileno: %s\n", strerror(errno));
+		return(HTML_FILE_LOCKERR);
+	}
+
+	if(lockf(fd, F_TEST, 0) < 0)
+		return(HTML_FILE_UNLOCKED);
+
+	return(HTML_FILE_UNLOCKED);
+}
+
+/* ---[DAEMONIZE]------------------------------------------------------------------------------------------ */
+/* void signal_handlerWithoutLock(int sig)
+ *
+ * Daemon signal handler (this function has no 'process lock file' to unlock).
+ *
+ * INPUT:
+ *  sig - signal received
+ * OUTPUT:
+ *  none
+ */
+void signal_handlerWithoutLock(int sig)
+{
+	fprintf(stderr, "Got signal [%d] at [%s]!\n", sig, time_DDMMYYhhmmss());
+
+	switch(sig){
+		case SIGHUP:
+			break;
+		case SIGTERM:
+			break;
+	}
+
+	exit(0);
+	return;
+}
+
+pid_t daemonizeWithoutLock(void)
+{
+	pid_t i = 0, father = 0;
+
+	father = getppid();
+	if(father == 1)
+		return(NOK);
+
+	i = fork();
+
+	father = i;
+
+	if(i == -1){
+		fprintf(stderr, "Erro fork chield process! [%s]\n", strerror(errno));
+		return((pid_t)NOK);
+	}
+
+	if(i > 0)
+		exit(0); /* Pai termina: 0 - Ok */
+
+	/* Continuando filho... */
+	setsid(); /* novo grupo */
+
+	umask(027);
+
+	chdir("./");
+
+	/* Configurando sinais */
+	signal(SIGCHLD, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGTTOU, SIG_IGN);
+	signal(SIGTTIN, SIG_IGN);
+	signal(SIGHUP, signal_handlerWithoutLock);
+	signal(SIGTERM, signal_handlerWithoutLock);
+	signal(SIGPIPE, signal_handlerWithoutLock);
+
+	return(getpid());
+}
+
+/* ---[LOG SYSTEM]----------------------------------------------------------------------------------------- */
+int log_open(char *logFile)
+{
+	if(logPath[0] != '\0'){
+		fprintf(stderr, "Log already initialized to: [%s]!\n", logFile);
+		return(NOK);
+	}
+
+	memset(logPath, 0, sizeof(logPath));
+	snprintf(logPath, LOGPATH_SZ, "%s/%s/%s", getPAINELEnvHomeVar(), LOG_PATH, logFile);
+
+	return(OK);
+}
+
+int log_close(void)
+{
+	memset(logPath, 0, sizeof(logPath));
+	return(OK);
+}
+
+int log_write(char *msg, ...)
+{
+	FILE *log = NULL;
+	char logText[MAXLINE] = {'\0'};
+	va_list args;
+
+	if((log = fopen(logPath, "a")) == NULL){
+		fprintf(stderr, "Unable to open/create log file to write [%s]! [%s]\n", logPath, strerror(errno));
+		return(NOK);
+	}
+	setbuf(log, NULL);
+
+	va_start(args, msg);
+
+	vsnprintf(logText, MAXLINE, msg, args);
+
+	va_end(args);
+
+	fprintf(log, "%s", logText);
+
+	fflush(log);
+	fclose(log);
+
+	return(OK);
+}
