ed6c5183a93eae6fe95b3e4ccbf9a95386e489d3
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-02-09 20:15:19 -0200

    Add files via upload

---
+++ include/db.h
@@ -0,0 +1,51 @@
+/* Andre Augusto Giannotti Scota (a2gs)
+ * andre.scota@gmail.com
+ *
+ * PAINEL
+ *
+ * Public Domain
+ *
+ */
+
+
+/* db.h
+ * Database defines.
+ *
+ *  Who     | When       | What
+ *  --------+------------+----------------------------
+ *   a2gs   | 13/08/2018 | Creation
+ *          |            |
+ */
+
+
+#ifndef __DB_H__
+#define __DB_H__
+
+
+/* *** INCLUDES ****************************************************** */
+
+
+/* *** DEFINES ******************************************************* */
+#define DATABASE_PATH				("database")
+#define DATABASE_FILE				("database.db")
+#define DB_PATHFILE_SZ				(400)
+
+
+/* *** EXTERNS / LOCAL / GLOBALS VARIEBLES *************************** */
+
+
+/* *** DATA TYPES **************************************************** */
+
+
+/* *** INTERFACES / PROTOTYPES *************************************** */
+/* <header function description included by another sources> void function(void)
+ *
+ * <Description>
+ *
+ * INPUT:
+ *  <None>
+ * OUTPUT:
+ *  <None>
+ */
+
+#endif
---
+++ include/html.h
@@ -0,0 +1,49 @@
+/* Andre Augusto Giannotti Scota (a2gs)
+ * andre.scota@gmail.com
+ *
+ * PAINEL
+ *
+ * Public Domain
+ *
+ */
+
+
+/* html.h
+ * HTML defines.
+ *
+ *  Who     | When       | What
+ *  --------+------------+----------------------------
+ *   a2gs   | 13/08/2018 | Creation
+ *          |            |
+ */
+
+
+#ifndef __HTML_H__
+#define __HTML_H__
+
+
+/* *** INCLUDES ****************************************************** */
+
+
+/* *** DEFINES ******************************************************* */
+#define HTML_PATH				("html")
+
+
+/* *** EXTERNS / LOCAL / GLOBALS VARIEBLES *************************** */
+
+
+/* *** DATA TYPES **************************************************** */
+
+
+/* *** INTERFACES / PROTOTYPES *************************************** */
+/* <header function description included by another sources> void function(void)
+ *
+ * <Description>
+ *
+ * INPUT:
+ *  <None>
+ * OUTPUT:
+ *  <None>
+ */
+
+#endif
