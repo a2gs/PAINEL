7a3346c717a500f12f9d5ed48f9bb23a2221800a
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-03-28 15:55:15 -0300

    runAll.sh servList number of parameters corrction

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -48,28 +48,28 @@ sleep 1
 
 # ------------------------------------------------
 echo '--- Stating servLists ---------------------------------'
-$PAINEL_HOME/bin/servList 9997 $PAINEL_HOME/html/All_Refresh.html               2> $PAINEL_HOME/log/allRefresh.log
+$PAINEL_HOME/bin/servList 9997 $PAINEL_HOME/html/All_Refresh.html $PAINEL_HOME/log/allRefresh.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV'
 ALERT_ERROR 'servList 9997 allRefresh.log'
 
-$PAINEL_HOME/bin/servList 9996 $PAINEL_HOME/html/All_Static.html                2> $PAINEL_HOME/log/all.log
+$PAINEL_HOME/bin/servList 9996 $PAINEL_HOME/html/All_Static.html $PAINEL_HOME/log/all.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV'
 ALERT_ERROR 'servList 9996 all.log'
 
-$PAINEL_HOME/bin/servList 9995 $PAINEL_HOME/html/FornoEletrico_Refresh.html     2> $PAINEL_HOME/log/FornoEletricoRefresh.log
+$PAINEL_HOME/bin/servList 9995 $PAINEL_HOME/html/FornoEletrico_Refresh.html $PAINEL_HOME/log/FornoEletricoRefresh.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV'
 ALERT_ERROR 'servList 9995 FornoEletricoRefresh.log'
 
-$PAINEL_HOME/bin/servList 9994 $PAINEL_HOME/html/FornoEletrico_Static.html      2> $PAINEL_HOME/log/FornoEletrico.log
+$PAINEL_HOME/bin/servList 9994 $PAINEL_HOME/html/FornoEletrico_Static.html $PAINEL_HOME/log/FornoEletrico.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV'
 ALERT_ERROR 'servList 9994 FornoEletrico.log'
 
-$PAINEL_HOME/bin/servList 9993 $PAINEL_HOME/html/OperadorMaquina_Refresh.html   2> $PAINEL_HOME/log/OperadorMaquinaRefresh.log
+$PAINEL_HOME/bin/servList 9993 $PAINEL_HOME/html/OperadorMaquina_Refresh.html $PAINEL_HOME/log/OperadorMaquinaRefresh.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV'
 ALERT_ERROR 'servList 9993 OperadorMaquinaRefresh.log'
 
-$PAINEL_HOME/bin/servList 9992 $PAINEL_HOME/html/OperadorMaquina_Static.html    2> $PAINEL_HOME/log/OperadorMaquina.log
+$PAINEL_HOME/bin/servList 9992 $PAINEL_HOME/html/OperadorMaquina_Static.html $PAINEL_HOME/log/OperadorMaquina.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV'
 ALERT_ERROR 'servList 9992 OperadorMaquina.log'
 
-$PAINEL_HOME/bin/servList 9991 $PAINEL_HOME/html/SupervisorMaquina_Refresh.html 2> $PAINEL_HOME/log/SupervisorMaquinaRefresh.log
+$PAINEL_HOME/bin/servList 9991 $PAINEL_HOME/html/SupervisorMaquina_Refresh.html $PAINEL_HOME/log/SupervisorMaquinaRefresh.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV'
 ALERT_ERROR 'servList 9991 SupervisorMaquinaRefresh.log'
 
-$PAINEL_HOME/bin/servList 9990 $PAINEL_HOME/html/SupervisorMaquina_Static.html  2> $PAINEL_HOME/log/SupervisorMaquina.log
+$PAINEL_HOME/bin/servList 9990 $PAINEL_HOME/html/SupervisorMaquina_Static.html $PAINEL_HOME/log/SupervisorMaquina.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV'
 ALERT_ERROR 'servList 9990 SupervisorMaquina.log'
 
 
