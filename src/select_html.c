a6cbf4796feb8b0c30f649372145108c54cd036b
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-04-02 18:56:18 -0300

    Improving the log 2

--- src/select_html.c
+++ src/select_html.c
@@ -298,6 +298,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "\tMSG = Just a message\n");
 		fprintf(stderr, "\tDEV = Developer (DEBUG) message\n\n");
 		fprintf(stderr, "PAINEL Home: [%s]\n", getPAINELEnvHomeVar());
+
 		return(-1);
 	}
 
@@ -309,6 +310,8 @@ int main(int argc, char *argv[])
 	p = daemonizeWithoutLock(&log);
 	if(p == (pid_t)NOK){
 		logWrite(&log, LOGMUSTLOGIT, "Cannt daemonize select html!\n");
+		logWrite(&log, LOGREDALERT, "Terminating application!\n");
+
 		logClose(&log);
 		return(-3);
 	}
@@ -328,6 +331,7 @@ int main(int argc, char *argv[])
 
 		if(dbOpen(NULL, SQLITE_OPEN_READONLY|SQLITE_OPEN_FULLMUTEX|SQLITE_OPEN_SHAREDCACHE, &log) == NOK){
 			logWrite(&log, LOGREDALERT, "Erro em abrir banco de dados!\n");
+			logWrite(&log, LOGREDALERT, "Terminating application!\n");
 
 			dbClose();
 			logClose(&log);
@@ -345,6 +349,7 @@ int main(int argc, char *argv[])
 
 		if(dbSelect(sql, hmtl_relat_infos, &pageInfo) == NOK){
 			logWrite(&log, LOGOPALERT, "Error at select database to define reports [%s]!\n", sql);
+			logWrite(&log, LOGREDALERT, "Terminating application!\n");
 
 			logClose(&log);
 			dbClose();
@@ -368,18 +373,24 @@ int main(int argc, char *argv[])
 		memset(&htmls, 0, sizeof(htmlFiles_t));
 		if(html_fopen(&htmls, fHtmlStatic, fHtmlRefresh) == NOK){
 			logWrite(&log, LOGOPALERT, "Falha em abrir/criar arquivos htmls [%s] e [%s].\n", fHtmlStatic, fHtmlRefresh);
+			logWrite(&log, LOGREDALERT, "Terminating application!\n");
+
 			logClose(&log);
 			return(-7);
 		}
 
 		if(html_header(&htmls, pageInfo.title, segRefresh) == NOK){
 			logWrite(&log, LOGOPALERT, "Falha em escrever header para arquivos htmls [%s] e [%s].\n", fHtmlStatic, fHtmlRefresh);
+			logWrite(&log, LOGREDALERT, "Terminating application!\n");
+
 			logClose(&log);
 			return(-8);
 		}
 
 		if(html_startTable(&htmls, pageInfo.columnsHeaders) == NOK){
 			logWrite(&log, LOGOPALERT, "Falha em escrever tabela para arquivos htmls [%s] e [%s].\n", fHtmlStatic, fHtmlRefresh);
+			logWrite(&log, LOGREDALERT, "Terminating application!\n");
+
 			logClose(&log);
 			return(-9);
 		}
@@ -397,6 +408,7 @@ int main(int argc, char *argv[])
 
 		if(dbSelect(sql, hmtl_constructTable, &htmls) == NOK){
 			logWrite(&log, LOGOPALERT, "Error at select database to construct tables [%s]!\n", sql);
+			logWrite(&log, LOGREDALERT, "Terminating application!\n");
 
 			logClose(&log);
 			dbClose();
@@ -411,6 +423,7 @@ int main(int argc, char *argv[])
     
 		if(html_endTable(&htmls) == NOK){
 			logWrite(&log, LOGOPALERT, "Falha em escrever finalizar arquivos htmls [%s] e [%s].\n", fHtmlStatic, fHtmlRefresh);
+			logWrite(&log, LOGREDALERT, "Terminating application!\n");
 
 			logClose(&log);
 			dbClose();
@@ -425,6 +438,8 @@ int main(int argc, char *argv[])
 
 		sleep(segReaload);
 	}
+
+	logWrite(&log, LOGREDALERT, "Terminating application with sucessfully!\n");
     
 	dbClose();
 	logClose(&log);
