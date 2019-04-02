8f183fa07f9ca9881e7771eb66691360c00ec928
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-04-01 22:53:23 -0300

    CREATE TABLE USERS e RELATS onde tem SELECT #18 - 2.2 - BROKEN

--- src/create_db.c
+++ src/create_db.c
@@ -52,12 +52,6 @@
 int main(int argc, char *argv[])
 {
 	log_t log;
-	/*
-	char DBPath[DB_PATHFILE_SZ + 1] = {'\0'};
-
-	snprintf(DBPath, DB_PATHFILE_SZ, "%s/%s/%s", getPAINELEnvHomeVar(), DATABASE_PATH, DATABASE_FILE);
-	fprintf(stderr, "Criando banco: [%s]\n", DBPath);
-	*/
 
 	if(argc != 3){
 		fprintf(stderr, "[%s %d] Usage:\n%s <LOG_FULL_PATH> <LOG_LEVEL 'WWW|XXX|YYY|ZZZ'>\n\n", time_DDMMYYhhmmss(), getpid(), argv[0]);
@@ -73,21 +67,27 @@ int main(int argc, char *argv[])
 		return(-1);
 	}
 
+	if(logCreate(&log, argv[1], argv[2]) == LOG_NOK){
+		fprintf(stderr, "[%s %d] Unable to open/create [%s]! [%s]\n", time_DDMMYYhhmmss(), getpid(), argv[2], strerror(errno));
+		return(-2);
+	}
+
 	if(dbOpen(NULL, SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE|SQLITE_OPEN_FULLMUTEX|SQLITE_OPEN_SHAREDCACHE, &log) == NOK){
 		logWrite(&log, LOGREDALERT, "Erro em abrir/criar banco de dados!\n");
+		logClose(&log);
+		return(-3);
+	}
+
+	if(createAllTables(DBPath) == NOK){
+		logWrite(&log, LOGREDALERT, "ERRO criando banco de dados (tabelas) SQLite!\n");
 		logClose(&log);
 		return(-4);
 	}
 
-
-	if(createAllTables(DBPath) == NOK){
-		fprintf(stderr, "ERRO criando banco de dados (tabelas) SQLite!\n");
-		return(-1);
-	}
-
-	dbClose();
-
 	fprintf(stderr, "Banco de dados (tabelas) SQLite criado!\n");
+
+	logClose(&log);
+	dbClose();
 
 	return(0);
 }
