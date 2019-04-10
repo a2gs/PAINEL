b4f7dc0b8115a572db566dac582d10dba86e1271
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-04-10 02:01:07 -0300

    Trace parsing

--- src/SG_serv.c
+++ src/SG_serv.c
@@ -104,88 +104,132 @@ int SG_parsingDataInsertRegistro(char *msg, char *ip, int port, SG_registroDB_t 
 	cutter(&p, '|', data->drt, DRT_LEN);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui1\n");
+
 	/* DATAHORA */
 	cutter(&p, '|', data->data, DATA_LEN);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui2\n");
 
 	/* LOGINOUT */
 	cutter(&p, '|', data->loginout, 1);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui3\n");
+
 	/* FUNCAO */
 	cutter(&p, '|', data->funcao, VALOR_FUNCAO_LEN);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui4\n");
 
 	/* PANELA */
 	cutter(&p, '|', data->panela, FORNELET_PAMELA_LEN);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui5\n");
+
 	/* WS */
 	cutter(&p, '|', data->ws, OPEMAQ_WS_LEN);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui6\n");
 
 	/* FORNELETR */
 	cutter(&p, '|', data->fornEletr, FORNELET_NUMFORELE_LEN);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui7\n");
+
 	/* NUMMAQUINA */
 	cutter(&p, '|', data->numMaquina, OPEMAQ_NUMMAQ_LEN);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui8\n");
 
 	/* DIAMETRO */
 	cutter(&p, '|', data->diamNom, OPEMAQ_DIAMNOM_LEN);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui9\n");
+
 	/* CLASSE */
 	cutter(&p, '|', data->classe, OPEMAQ_CLASSE_LEN);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui10\n");
 
 	/* TEMP */
 	cutter(&p, '|', data->temp, OPEMAQ_TEMP_LEN);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui11\n");
+
 	/* PERCFESI */
 	cutter(&p, '|', data->percFeSi, OPEMAQ_PERC_FESI_LEN);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui12\n");
 
 	/* PERCMG */
 	cutter(&p, '|', data->percMg, FORNELET_PERC_MG_LEN);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui13\n");
+
 	/* PERCC */
 	cutter(&p, '|', data->percC, FORNELET_PERC_C_LEN);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui14\n");
 
 	/* PERCS */
 	cutter(&p, '|', data->percS, FORNELET_PERC_S_LEN);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui15\n");
+
 	/* PERCP */
 	cutter(&p, '|', data->percP, FORNELET_PERC_P_LEN);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui16\n");
 
 	/* PERCINOCLNT */
 	cutter(&p, '|', data->percInoculante, OPEMAQ_PERC_INOC_LEN);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui17\n");
+
 	/* ENELETTON */
 	cutter(&p, '|', data->enerEletTon, OPEMAQ_ENEELETON_LEN);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui18\n");
 
 	/* CADENCIA */
 	cutter(&p, '|', data->cadencia, SUPMAQ_CADENCIA);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui19\n");
+
 	/* OEE */
 	cutter(&p, '|', data->oee, SUPMAQ_OEE);
 	if(*p == '\0') return(NOK);
+
+	logWrite(log, LOGDEV, "aqui20\n");
 
 	/* ASPECTUBO */
 	cutter(&p, '|', data->aspecto, SUPMAQ_ASPEC_LEN);
 	if(*p == '\0') return(NOK);
 
+	logWrite(log, LOGDEV, "aqui21\n");
+
 	/* REFUGO (ultimo) */
 	cutter(&p, '|', data->refugo, SUPMAG_REFUGO_LEN);
+
+	logWrite(log, LOGDEV, "aqui22\n");
 
 	/* CLIENT IP/PORT */
 	snprintf(data->ipport, VALOR_IPPORT_LEN, "%s:%d", ip, port);
