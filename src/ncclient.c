545986fab803f337166e69ff0c156f4e2766c106
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-06-12 23:14:59 -0300

    NCurse client #26 (login screen) 21.3

--- src/ncclient.c
+++ src/ncclient.c
@@ -423,6 +423,8 @@ a2gs_ToolBox_WizardReturnFunc_t screen_login(void *data)
 		goto CLEANUP_SCREEN_LOGIN;
 	}
 
+	/* TODO: GET LEVEL HERE */
+
 	while(1){
 		post_form(formLogin);
 
@@ -443,9 +445,11 @@ a2gs_ToolBox_WizardReturnFunc_t screen_login(void *data)
 			curs_set(0);
 
 			/*
-			 * if(Check if user/level are registred into DRTs.text) == OK{
-			 * 	User ok, get user IFACE cmd
-			 * 	GO TO USER (defined) DYNAMIC SCREEN
+			 * if(Check if user are registred into DRTs.text) == OK{
+			 *    if(check login to server == OK){
+			 * 		User ok, get user IFACE cmd
+			 * 		GO TO USER (defined) DYNAMIC SCREEN
+			 * 	}
 			 * }else{
 			 *
 			 * }
