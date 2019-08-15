7aea406d0748ac3cb0a2ff8ef00a39d59501b5e8
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-08-15 19:26:40 -0300

    servList runAll script corrections

--- scripts/runServList.sh
+++ scripts/runServList.sh
@@ -24,28 +24,36 @@ if [ -f "$PAINEL_HOME/bin/servList" ]; then
 
 	echo '--- Stating servLists ---------------------------------'
 
-	$PAINEL_HOME/bin/servList 9997 $PAINEL_HOME/html/All_Refresh.html $PAINEL_HOME/log/allRefresh_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	#$PAINEL_HOME/bin/servList 9997 $PAINEL_HOME/html/All_Refresh.html $PAINEL_HOME/log/allRefresh_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	$PAINEL_HOME/bin/servList $PAINEL_HOME/servList_7.cfg 1>>$PAINEL_HOME/log/servList.log 2>&1
 	ALERT_ERROR "servList 9997 allRefresh_$DATE.log"
 
-	$PAINEL_HOME/bin/servList 9996 $PAINEL_HOME/html/All_Static.html $PAINEL_HOME/log/all_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	#$PAINEL_HOME/bin/servList 9996 $PAINEL_HOME/html/All_Static.html $PAINEL_HOME/log/all_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	$PAINEL_HOME/bin/servList $PAINEL_HOME/servList_6.cfg 1>>$PAINEL_HOME/log/servList.log 2>&1
 	ALERT_ERROR "servList 9996 all_$DATE.log"
 
-	$PAINEL_HOME/bin/servList 9995 $PAINEL_HOME/html/FornoEletrico_Refresh.html $PAINEL_HOME/log/FornoEletricoRefresh_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	#$PAINEL_HOME/bin/servList 9995 $PAINEL_HOME/html/FornoEletrico_Refresh.html $PAINEL_HOME/log/FornoEletricoRefresh_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	$PAINEL_HOME/bin/servList $PAINEL_HOME/servList_5.cfg 1>>$PAINEL_HOME/log/servList.log 2>&1
 	ALERT_ERROR "servList 9995 FornoEletricoRefresh_$DATE.log"
 
-	$PAINEL_HOME/bin/servList 9994 $PAINEL_HOME/html/FornoEletrico_Static.html $PAINEL_HOME/log/FornoEletrico_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	#$PAINEL_HOME/bin/servList 9994 $PAINEL_HOME/html/FornoEletrico_Static.html $PAINEL_HOME/log/FornoEletrico_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	$PAINEL_HOME/bin/servList $PAINEL_HOME/servList_4.cfg 1>>$PAINEL_HOME/log/servList.log 2>&1
 	ALERT_ERROR "servList 9994 FornoEletrico_$DATE.log"
 
-	$PAINEL_HOME/bin/servList 9993 $PAINEL_HOME/html/OperadorMaquina_Refresh.html $PAINEL_HOME/log/OperadorMaquinaRefresh_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	#$PAINEL_HOME/bin/servList 9993 $PAINEL_HOME/html/OperadorMaquina_Refresh.html $PAINEL_HOME/log/OperadorMaquinaRefresh_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	$PAINEL_HOME/bin/servList $PAINEL_HOME/servList_3.cfg 1>>$PAINEL_HOME/log/servList.log 2>&1
 	ALERT_ERROR "servList 9993 OperadorMaquinaRefresh_$DATE.log"
 
-	$PAINEL_HOME/bin/servList 9992 $PAINEL_HOME/html/OperadorMaquina_Static.html $PAINEL_HOME/log/OperadorMaquina_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	#$PAINEL_HOME/bin/servList 9992 $PAINEL_HOME/html/OperadorMaquina_Static.html $PAINEL_HOME/log/OperadorMaquina_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	$PAINEL_HOME/bin/servList $PAINEL_HOME/servList_2.cfg 1>>$PAINEL_HOME/log/servList.log 2>&1
 	ALERT_ERROR "servList 9992 OperadorMaquina_$DATE.log"
 
-	$PAINEL_HOME/bin/servList 9991 $PAINEL_HOME/html/SupervisorMaquina_Refresh.html $PAINEL_HOME/log/SupervisorMaquinaRefresh_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	#$PAINEL_HOME/bin/servList 9991 $PAINEL_HOME/html/SupervisorMaquina_Refresh.html $PAINEL_HOME/log/SupervisorMaquinaRefresh_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	$PAINEL_HOME/bin/servList $PAINEL_HOME/servList_1.cfg 1>>$PAINEL_HOME/log/servList.log 2>&1
 	ALERT_ERROR "servList 9991 SupervisorMaquinaRefresh_$DATE.log"
 
-	$PAINEL_HOME/bin/servList 9990 $PAINEL_HOME/html/SupervisorMaquina_Static.html $PAINEL_HOME/log/SupervisorMaquina_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	#$PAINEL_HOME/bin/servList 9990 $PAINEL_HOME/html/SupervisorMaquina_Static.html $PAINEL_HOME/log/SupervisorMaquina_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/servList.log 2>&1
+	$PAINEL_HOME/bin/servList $PAINEL_HOME/servList_0.cfg 1>>$PAINEL_HOME/log/servList.log 2>&1
 	ALERT_ERROR "servList 9990 SupervisorMaquina_$DATE.log"
 
 else
