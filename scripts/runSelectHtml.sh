232630d05a34f8508b305826f3e864db97ffaa83
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-10-18 22:01:28 -0300

    Creating CFG and PID subdirectories into running dir 2

--- scripts/runSelectHtml.sh
+++ scripts/runSelectHtml.sh
@@ -25,25 +25,25 @@ if [ -f "$PAINEL_HOME/bin/select_html" ]; then
 
 	echo "Launching select_html OperadorMaquina select_html_OperadorMaquina.log"
 	#$PAINEL_HOME/bin/select_html OperadorMaquina 4 5 $PAINEL_HOME/log/select_html_OperadorMaquina_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/select_html.log 2>&1
-	$PAINEL_HOME/bin/select_html $PAINEL_HOME/running/select_html_OperadorMaquina.cfg 1>>$PAINEL_HOME/log/select_html.log 2>&1
+	$PAINEL_HOME/bin/select_html $PAINEL_HOME/running/CFGs/select_html_OperadorMaquina.cfg 1>>$PAINEL_HOME/log/select_html.log 2>&1
 	ALERT_ERROR "select_html OperadorMaquina select_html_OperadorMaquina_$DATE.log"
 	sleep 1
 
 	echo "Launching select_html SupervisorMaquina select_html_SupervisorMaquina_$DATE.log"
 	#$PAINEL_HOME/bin/select_html SupervisorMaquina 4 5 $PAINEL_HOME/log/select_html_SupervisorMaquina_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/select_html.log 2>&1
-	$PAINEL_HOME/bin/select_html $PAINEL_HOME/running/select_html_SupervisorMaquina.cfg 1>>$PAINEL_HOME/log/select_html.log 2>&1
+	$PAINEL_HOME/bin/select_html $PAINEL_HOME/running/CFGs/select_html_SupervisorMaquina.cfg 1>>$PAINEL_HOME/log/select_html.log 2>&1
 	ALERT_ERROR "select_html SupervisorMaquina select_html_SupervisorMaquina_$DATE.log"
 	sleep 1
 
 	echo "Launching select_html FornoEletrico select_html_FornoEletrico_$DATE.log"
 	#$PAINEL_HOME/bin/select_html FornoEletrico 4 5 $PAINEL_HOME/log/select_html_FornoEletrico_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/select_html.log 2>&1
-	$PAINEL_HOME/bin/select_html $PAINEL_HOME/running/select_html_FornoEletrico.cfg 1>>$PAINEL_HOME/log/select_html.log 2>&1
+	$PAINEL_HOME/bin/select_html $PAINEL_HOME/running/CFGs/select_html_FornoEletrico.cfg 1>>$PAINEL_HOME/log/select_html.log 2>&1
 	ALERT_ERROR "select_html FornoEletrico select_html_FornoEletrico.log"
 	sleep 1
 
 	echo "Launching select_html All select_html_All_$DATE.log"
 	#$PAINEL_HOME/bin/select_html All 4 5 $PAINEL_HOME/log/select_html_All_$DATE.log 'REDALERT|DBALERT|DBMSG|OPALERT|OPMSG|MSG|DEV' 1>>$PAINEL_HOME/log/select_html.log 2>&1
-	$PAINEL_HOME/bin/select_html $PAINEL_HOME/running/select_html_All.cfg 1>>$PAINEL_HOME/log/select_html.log 2>&1
+	$PAINEL_HOME/bin/select_html $PAINEL_HOME/running/CFGs/select_html_All.cfg 1>>$PAINEL_HOME/log/select_html.log 2>&1
 	ALERT_ERROR "select_html All select_html_All_$DATE.log"
 	sleep 1
 
