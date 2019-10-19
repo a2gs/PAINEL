edfa95d7adc364788ec899e19d9cc6cce4e157e9
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-10-18 22:40:45 -0300

    At listPIDs, list if there is a servlock

--- scripts/listPIDs.sh
+++ scripts/listPIDs.sh
@@ -20,6 +20,12 @@
 echo '--- PROCESSOS NO AR -----------------------------------'
 ps -C serv,servList,select_html -o pid,cmd | sed 's/^ *//' | column -t
 
+if [ -f "$PAINEL_HOME/running/PIDs/servlock" ]; then
+	echo '--- SERVLOCK ------------------------------------------'
+	echo "There is a servlock [$PAINEL_HOME/running/PIDs/servlock] file! Content:"
+	cat $PAINEL_HOME/running/PIDs/servlock
+fi
+
 echo '--- PORTAS EM LISTNING --------------------------------'
 PROCS_PID_LIST=$(ps -C serv,servList,select_html --no-headers -o pid,cmd | sed 's/^ *//' | cut -f1 -d ' ')
 
